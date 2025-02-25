42d71f908caffb40432e90ffa3d822832b1c2952
Federico <fedealtava1992@gmail.com>
2025-02-25 20:37:38 +0100

    intem task done

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,8 +1,14 @@
-export const TaskItem =(task)=>{
+import { Task } from "../interfaces/Task";
+export const TaskItem =({ task }: { task: Task })=>{
     const { title, description, completed, id } = task;
     return(
         <div>
-
+            <div className="cardTask">
+                <h1>{title}</h1>
+                <p>{description}</p>
+                {completed && <p>Completada</p>}
+                <p>Fecha de creacion {id}</p>
+            </div>
         </div>
     )
 }
